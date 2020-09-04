d4a87a2bce6967758d9fe450612451da4ecfd26f
Arend de Jonge <arend@crownstone.rocks>
2020-09-04 14:40:49 +0200

    Move commenting from SimulatorCrownstone to CrownstoneCore

--- examples/originalExample/exampleCrownstones/SimulatorCrownstone.py
+++ examples/originalExample/exampleCrownstones/SimulatorCrownstone.py
@@ -4,36 +4,19 @@ from simulator.simulatorBases.GuiCrownstoneCore import GuiCrownstoneCore
 
 class SimulatorCrownstone(GuiCrownstoneCore):
     
-    """
-        Class variables are created here.
-    """
-    
     def __init__(self, id, x, y):
         super().__init__(id=id,x=x,y=y)
         self.myValue = False
        
     def resetState(self, resetTrainingData = True):
-        """
-            This is an important method to reset any state the Crownstone may have so the simulation can be restarted.
-            If resetTrainingData is False, you should clear all state data except that referring to the training sets.
-        """
         self.myValue = False
-    
-       
+
     # overloaded
     def receiveMessage(self, data, rssi):
-        """
-            This is where mesh messages are received
-            :param data:  { "sender":string, "payload": dictionary }
-        """
         print(self.id, "I HAVE A MESSAGE FROM", data["sender"], " SAYING ", data["payload"])
 
     # overloaded
     def newMeasurement(self, data, rssi):
-        """
-            This is where scanned ble devices are seen
-            :param data:  { "address":string, "payload": dictionary }
-        """
         print(self.time, self.id, "Scans indicate", data["address"], " with payload ", data["payload"], " and rssi:", rssi)
         
         if rssi > -45:
--- simulator/simulatorBases/CrownstoneCore.py
+++ simulator/simulatorBases/CrownstoneCore.py
@@ -23,9 +23,17 @@ class CrownstoneCore:
         self.eventBus.emit(Topics.meshMessage, {"sender": self.id, "payload": data, "ttl": ttl, "repeat": repeat})
     
     def receiveMessage(self, data, rssi):
+        """
+            This is where mesh messages are received
+            :param data:  { "sender":string, "payload": dictionary }
+        """
         pass
     
     def newMeasurement(self, data, rssi):
+        """
+            This is where scanned ble devices are seen
+            :param data:  { "address":string, "payload": dictionary }
+        """
         pass
     
     def tick(self, time):
@@ -35,4 +43,8 @@ class CrownstoneCore:
         self.eventBus.emit(Topics.gotResult, {"sender": self.id, "roomId": roomId})
         
     def resetState(self, resetTrainingData = True):
+        """
+            This is an important method to reset any state the Crownstone may have so the simulation can be restarted.
+            If resetTrainingData is False, you should clear all state data except that referring to the training sets.
+        """
         pass
